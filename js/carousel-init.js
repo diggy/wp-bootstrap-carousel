68dce48af0bdbeb97367d8f6786f48551f1a7ae7
diggy <peterherrel@gmail.com>
2012-11-14 01:11:57 +0100

    initial commit

---
+++ js/carousel-init.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){
+	if(jQuery(".carousel-inner-dps").length){jQuery(".carousel-inner-dps div:not(:first)").removeClass("active");}
+	jQuery(".carousel").carousel({interval:wp_bootstrap_carousel_js_vars.interval,pause:wp_bootstrap_carousel_js_vars.pause});
+});
