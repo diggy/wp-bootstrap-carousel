840fa121732079b9cab9e8ad2bfa15752ed5fe35
diggy <peterherrel@gmail.com>
2014-10-12 20:24:43 +0200

    0.3.0 init JS minified

--- js/carousel-init.js
+++ js/carousel-init.js
@@ -1,8 +1,15 @@
 jQuery(document).ready(function($){
-	if($(".carousel-inner-dps").length){
-		$(".carousel-inner-dps").each(function(){
-			$(this).find("div:not(:first)").removeClass("active");
-		});
-	}
-	$(".carousel").carousel();
+    if($(".carousel-inner").length){
+        $(".carousel-inner .item img").each(function(){
+            if($(this).parent().is( "a" ) && $(this).attr('data-wpbc_unwrap')) {
+                $(this).unwrap();
+            }
+        });
+    }
+    if($(".carousel-inner-dps").length){
+        $(".carousel-inner-dps").each(function(){
+            $(this).find("div:not(:first)").removeClass("active");
+        });
+    }
+    $(".carousel").carousel();
 });
